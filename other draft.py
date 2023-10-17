f08877946b3e375e2f727b04e74701184f71794f
tingyup1 <142997103+tingyup1@users.noreply.github.com>
2023-10-17 22:29:14 +0300

    Delete other draft.py

--- other draft.py
+++
@@ -1,9 +0,0 @@
-x = int(input("How much cash you have?"))
-asw = input("Can I look your wallet(y/n)?")
-
-if(x>100000 and asw == 'y'):
-    print("You are rich")
-elif(x>10):
-        print("You have more than 10 euros")
-else:
-    print("You are poor")
